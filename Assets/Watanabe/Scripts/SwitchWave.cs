d795e49672c9de1275fa585b4f6bcce1639a127f
Akira Watanabe <a0323w@gmail.com>
2022-11-23 13:50:35 +0900

    [add] 切り替えクラス、プレハブ

--- Assets/Watanabe/Scripts/SwitchWave.cs
+++ Assets/Watanabe/Scripts/SwitchWave.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 /// </summary>
 public class SwitchWave : MonoBehaviour
 {
+    [Tooltip("切り替えオブジェクトに入ってきたのがPlayerかどうか")]
+    [SerializeField, TagName] string _enterTag;
+
     /// <summary> 寒波か、熱波か(false...寒波, true...熱波) </summary>
     public bool IsWarm { get; set; }
 
@@ -17,5 +20,16 @@ public class SwitchWave : MonoBehaviour
         IsWarm = IsWarm == true ? false : true;
         Debug.Log(IsWarm);
         //playerに、現在の攻撃の状態を保存しておく変数を用意してもらう
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("切り替えます");
+        //切り替えオブジェクトに入ってきたのがPlayerだったら
+        if (col.tag == _enterTag)
+        {
+            //寒波、熱波を切り替える
+            Switch();
+        }
     }
 }
